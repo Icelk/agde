439efc12bca184009f536c9baef9197fc432c8e0
Icelk <main@icelk.dev>
2021-11-11 12:28:52 +0100

    Continued implementation.

--- src/lib.rs
+++ src/lib.rs
@@ -5,12 +5,44 @@
     missing_docs
 )]
 
-pub enum Event {
-    Modify(ModifyEvent),
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, PartialEq, Eq, Clone, Serialize, Deserialize)]
+pub struct ModifyEvent<'a> {
+    resource: &'a str,
+    section: Section<'a>,
+    source: Option<&'a [u8]>,
+}
+impl<'a> ModifyEvent<'a> {
+    pub fn new(resource: &'a str, section: Section<'a>, source: Option<&'a [u8]>) -> Self {
+        Self {
+            resource,
+            section,
+            source,
+        }
+    }
+}
+
+macro_rules! into_event {
+    ($type:ty, $enum_name:ident) => {
+        impl<'a> From<$type> for Event<'a> {
+            fn from(event: $type) -> Self {
+                Self::$enum_name(event)
+            }
+        }
+    };
+}
+
+into_event!(ModifyEvent<'a>, Modify);
+
+#[derive(Debug, PartialEq, Eq, Clone, Serialize, Deserialize)]
+pub enum Event<'a> {
+    Modify(ModifyEvent<'a>),
     Delete(DeleteEvent),
     Move(MoveEvent),
 }
 
+#[derive(Debug, PartialEq, Eq, Clone, Serialize, Deserialize)]
 pub struct Section<'a> {
     /// The start of the previous data in the resource.
     start: usize,
@@ -19,10 +51,48 @@ pub struct Section<'a> {
     /// A reference to the data.
     data: &'a [u8],
 }
-
 impl<'a> Section<'a> {
     pub fn new(start: usize, end: usize, data: &'a [u8]) -> Self {
         Self { start, end, data }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Serialize, Deserialize)]
+pub struct Message<'a> {
+    events: Vec<Event<'a>>,
+    uuid: u64,
+}
+impl<'a> Message<'a> {
+    pub fn new(events: Vec<Event<'a>>) -> Self {
+        Self::with_uuid(events, rand::random())
+    }
+    pub fn with_uuid(events: Vec<Event<'a>>, uuid: u64) -> Self {
+        Self { events, uuid }
+    }
+    pub fn with_rng(events: Vec<Event<'a>>, rng: impl rand::Rng) -> Self {
+        Self::with_uuid(events, rng.gen())
+    }
+    pub fn event_iter(&self) -> impl Iterator<Item = &Event> {
+        self.events.iter()
+    }
+}
+
+pub struct Manager {
+    rng: rand::rngs::ThreadRng,
+}
+impl Manager {
+    pub fn new() -> Self {
+        Self::default()
+    }
+    pub fn process<'a>(&mut self, event: Event<'a>) -> Message<'a> {
+        Message::with_rng(vec![event], &mut self.rng)
+    }
+}
+impl Default for Manager {
+    fn default() -> Self {
+        Self {
+            rng: rand::thread_rng(),
+        }
     }
 }
 
@@ -33,17 +103,20 @@ mod tests {
     fn send_diff() {
         let mut manager = Manager::default();
 
-        let event = ModifyEvent::new("test.txt", Section::new(0, 0, b"Some test data."));
+        let event = ModifyEvent::new("test.txt", Section::new(0, 0, b"Some test data."), None);
 
-        let mut action_iter = manager.process(event.clone().into());
+        let mut message = manager.process(event.clone().into());
 
-        let actions = action_iter.collect();
+        let actions: Vec<_> = message.event_iter().collect();
 
         // The actions are sent to a different client.
 
         let mut receiver = Manager::default();
 
-        let mut events = actions.into_iter().map(|action| receiver.apply(action)).flatten();
+        let mut events = actions
+            .into_iter()
+            .map(|action| receiver.apply(action))
+            .flatten();
 
         assert_eq!(events.next(), Some(event));
         assert_eq!(events.next(), None);
@@ -53,7 +126,7 @@ mod tests {
     }
 
     // Test doing this ↑ but simplifying as it had previous data, stored how?
-    
+
     // Test when the underlying data has changed without events; then this library is called again.
     // A special call to the library, which will request all the files, mtime & size to see which
     // have changed.
