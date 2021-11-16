5f1026bce407fa0bdda125fcd6560bf6f89ae917
Icelk <main@icelk.dev>
2021-11-16 20:08:17 +0100

    Layed most of the foundations.

--- den/src/lib.rs
+++ den/src/lib.rs
@@ -1,38 +1,155 @@
+//! A difference library similar to `rsync`.
+
+#![deny(
+    clippy::pedantic,
+    unreachable_pub,
+    missing_debug_implementations,
+    // missing_docs
+)]
+
 use serde::{Deserialize, Serialize};
-use twox_hash::xxh3::HasherExt;
+use std::fmt::Debug;
 use std::hash::Hasher;
+use twox_hash::xxh3::HasherExt;
 
-// macro_rules! hashers {
-// () => {
-// None4, None8, None16, Fnv, XXH3_64, XXH3_128,
-// };
-// }
-
-pub enum HashBuilder {
-    None4([u8; 4], u8),
-    None8([u8; 8], u8),
-    None16([u8; 16], u8),
-    Fnv(fnv::FnvHasher),
-    XXH3_64(twox_hash::Xxh3Hash64),
-    XXH3_128(twox_hash::Xxh3Hash128),
+macro_rules! hashers {
+    ($macro: tt!) => {
+        $macro!(
+            (None4, [u8; 4], StackSlice<4>, finish),
+            (None8, [u8; 8], StackSlice<8>, finish),
+            (None16, [u8; 16], StackSlice<16>, finish),
+            (Fnv, [u8; 8], fnv::FnvHasher, finish),
+            (XXH3_64, [u8; 8], twox_hash::Xxh3Hash64, finish),
+            (XXH3_128, [u8; 16], twox_hash::Xxh3Hash128, finish_ext),
+        );
+    };
 }
-impl HashBuilder {
-    pub fn finish(self) -> HashAlgorithm {
-        use HashBuilder::*;
-        match self {
-            None4(v, _) => HashAlgorithm::None4(v),
-            None8(v, _) => HashAlgorithm::None8(v),
-            None16(v, _) => HashAlgorithm::None16(v),
-            Fnv(h) =>HashAlgorithm::Fnv(h.finish().to_le_bytes()),
-            XXH3_64(h) =>HashAlgorithm::XXH3_64(h.finish().to_le_bytes()),
-            XXH3_128(h) =>HashAlgorithm::XXH3_128(h.finish_ext().to_le_bytes()),
+
+macro_rules! hash_algorithm {
+    (
+        $(
+            (
+                $name: ident,
+                $result: ty,
+                $builder: ty,
+                $finish: ident
+            ),
+        )+
+    ) => {
+        #[derive(Debug, PartialEq, Eq, Clone, Copy, Deserialize, Serialize)]
+        #[must_use]
+        pub enum HashAlgorithm {
+            $(
+                $name,
+            )+
         }
+        impl HashAlgorithm {
+            fn builder(&self) -> HashBuilder {
+                match self {
+                    $(
+                        Self::$name => HashBuilder::$name(Default::default()),
+                    )+
+                }
+            }
+        }
+    };
+}
+
+hashers!(hash_algorithm!);
+
+macro_rules! hash_builder {
+    (
+        $(
+            (
+                $name: ident,
+                $result: ty,
+                $builder: ty,
+                $finish: ident
+            ),
+        )+
+    ) => {
+        enum HashBuilder {
+            $(
+                $name($builder),
+            )+
+        }
+
+        impl HashBuilder {
+            fn finish(self) -> HashResult {
+                match self {
+                    $(
+                        Self::$name(hasher) => HashResult::$name(hasher.$finish().to_le_bytes()),
+                    )+
+                }
+            }
+            fn write(&mut self, data: &[u8]) {
+
+                match self {
+                    $(
+                        Self::$name(hasher) => hasher.write(data),
+                    )+
+                }
+            }
+        }
+    };
+}
+
+hashers!(hash_builder!);
+
+impl Debug for HashBuilder {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str("HashBuilder (internal hasher data)")
+    }
+}
+
+#[derive(Debug)]
+struct StackSlice<const SIZE: usize> {
+    data: [u8; SIZE],
+    len: u8,
+}
+impl<const SIZE: usize> Default for StackSlice<SIZE> {
+    fn default() -> Self {
+        Self {
+            data: zeroed(),
+            len: 0,
+        }
+    }
+}
+impl<const SIZE: usize> StackSlice<SIZE> {
+    /// # Panics
+    ///
+    /// Panics if `data.len()` > [`Self::available`].
+    #[allow(clippy::cast_possible_truncation)]
+    fn write(&mut self, data: &[u8]) {
+        assert!(data.len() <= self.available());
+        self.data[self.len as usize..self.len as usize + data.len()].copy_from_slice(data);
+
+        // The assert above guarantees we never reach this point if the input is too large.
+        self.len += data.len() as u8;
+    }
+    fn available(&self) -> usize {
+        SIZE - self.len as usize
+    }
+}
+impl StackSlice<4> {
+    fn finish(self) -> u32 {
+        u32::from_ne_bytes(self.data)
+    }
+}
+impl StackSlice<8> {
+    fn finish(self) -> u64 {
+        u64::from_ne_bytes(self.data)
+    }
+}
+impl StackSlice<16> {
+    fn finish(self) -> u128 {
+        u128::from_ne_bytes(self.data)
     }
 }
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Serialize, Deserialize)]
 #[repr(u8)]
-pub enum HashAlgorithm {
+enum HashResult {
     None4([u8; 4]),
     None8([u8; 8]),
     None16([u8; 16]),
@@ -40,51 +157,87 @@ pub enum HashAlgorithm {
     XXH3_64([u8; 8]),
     XXH3_128([u8; 16]),
 }
-impl HashAlgorithm {
-    fn same_algo(&self) -> HashBuilder {
-        use HashAlgorithm::*;
 
-        match self {
-            None4(_) => HashBuilder::None4(Default::default(), 0),
-            None8(_) => HashBuilder::None8(Default::default(), 0),
-            None16(_) => HashBuilder::None16(Default::default(), 0),
-            Fnv(_) => HashBuilder::Fnv(Default::default()),
-            XXH3_64(_) => HashBuilder::XXH3_64(Default::default()),
-            XXH3_128(_) => HashBuilder::XXH3_128(Default::default()),
-        }
-    }
-}
-
-pub const fn zeroed<const SIZE: usize>() -> [u8; SIZE] {
+#[must_use]
+const fn zeroed<const SIZE: usize>() -> [u8; SIZE] {
     [0; SIZE]
 }
 
-#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
-pub struct Signature {
-    hasher: HashAlgorithm,
-    hashes: Vec<HashAlgorithm>,
+#[derive(Debug)]
+#[must_use]
+pub struct SignatureBuilder {
+    algo: HashAlgorithm,
+    blocks: Vec<HashResult>,
+    block_size: usize,
+
+    current: HashBuilder,
+    len: usize,
 }
-impl Signature {
+impl SignatureBuilder {
     /// The `hasher` is used as the template hasher from which all other hashers are cloned.
-    fn new(hasher: HashAlgorithm) -> Self {
+    fn new(algo: HashAlgorithm) -> Self {
         Self {
-            hasher,
-            hashes: Vec::new(),
+            algo,
+            blocks: Vec::new(),
+            block_size: 1024,
+
+            current: algo.builder(),
+            len: 0,
         }
     }
-    pub fn fnv() -> Self {
-        Self::new(HashAlgorithm::Fnv(zeroed()))
+    fn block_available(&self) -> usize {
+        self.block_size - self.len
     }
-    pub fn xxh3_64() -> Self {
-        Self::new(HashAlgorithm::XXH3_64(zeroed()))
+    fn finish_hash(&mut self) {
+        let builder = std::mem::replace(&mut self.current, self.algo.builder());
+        let result = builder.finish();
+        self.blocks.push(result);
+        self.len = 0;
     }
-    pub fn xxh3_128() -> Self {
-        Self::new(HashAlgorithm::XXH3_128(zeroed()))
+    pub fn write(&mut self, data: &[u8]) {
+        let mut data = data;
+
+        while data.len() > self.block_available() {
+            let bytes = &data[..self.block_available()];
+            self.current.write(bytes);
+
+            data = &data[self.block_available()..];
+            self.finish_hash();
+        }
     }
-    pub fn hash(&self) -> HashBuilder {
-        self.hasher.same_algo()
+    /// Sets the block size of the hashes to be `block_size` bytes.
+    ///
+    /// The default is `1024`.
+    fn with_block_size(mut self, block_size: usize) -> Self {
+        self.block_size = block_size;
+        self
     }
-    pub fn add(&mut self, hash: HashBuilder) {
-        self.hashes.push(hash.finish())
+    pub fn finish(mut self) -> Signature {
+        self.finish_hash();
+
+        let Self {
+            algo,
+            blocks,
+            block_size,
+            ..
+        } = self;
+
+        Signature {
+            algo,
+            blocks,
+            block_size,
+        }
+    }
+}
+#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[must_use]
+pub struct Signature {
+    algo: HashAlgorithm,
+    blocks: Vec<HashResult>,
+    block_size: usize,
+}
+impl Signature {
+    pub fn with_algo(algo: HashAlgorithm, block_size: usize) -> SignatureBuilder {
+        SignatureBuilder::new(algo).with_block_size(block_size)
     }
 }
