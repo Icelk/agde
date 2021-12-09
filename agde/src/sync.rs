d42042aedc6ee00a12555f335b8fc25389896b9d
Icelk <main@icelk.dev>
2021-12-09 20:30:20 +0100

    Changed sync signature to a HashMap.

--- agde/src/sync.rs
+++ agde/src/sync.rs
@@ -2,16 +2,20 @@
 //!
 //! This occurs when the hashes don't line up or when fast forwarding on a new connection.
 
+use std::collections::HashMap;
+
 use serde::{Deserialize, Serialize};
 
 use crate::{resource, Uuid};
 
+// `TODO`: Resolve multiple diffs. Enum for remove/create with data/den::Diff
+// `TODO`: apply the response, returning a iterator of the enum above?
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[must_use]
 pub struct Request {
     pier: Uuid,
     resources: resource::Matcher,
-    signature: den::Signature,
+    signature: HashMap<String, den::Signature>,
 }
 impl Request {}
 impl PartialEq for Request {
@@ -25,19 +29,28 @@ impl Eq for Request {}
 pub struct RequestBuilder {
     pier: Uuid,
     resources: resource::Matcher,
+    signature: HashMap<String, den::Signature>,
 }
 impl RequestBuilder {
     pub(crate) fn new(pier: Uuid, resources: resource::Matcher) -> Self {
-        Self { pier, resources }
+        Self {
+            pier,
+            resources,
+            signature: HashMap::new(),
+        }
+    }
+    pub fn insert(&mut self, resource: String, signature: den::Signature) -> &mut Self {
+        self.signature.insert(resource, signature);
+        self
     }
     /// Make a [`Request`] from this builder and a signature of all the resources matched using
     /// [`Self::matches`].
     #[inline]
-    pub fn finish(self, signature: den::Signature) -> Request {
+    pub fn finish(self) -> Request {
         Request {
             pier: self.pier,
             resources: self.resources,
-            signature,
+            signature: self.signature,
         }
     }
     /// Test if this `resource` should be part of the `signature` in [`Self::finish`].
