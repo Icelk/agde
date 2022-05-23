6d626957c070c2b86c59e63215f7e9ff8d8dcb56
Icelk <main@icelk.dev>
2022-05-23 19:30:40 +0200

    Added command line parsing for agde-tokio.

--- agde-tokio/src/bin/main.rs
+++ agde-tokio/src/bin/main.rs
@@ -1,23 +1,102 @@
 use agde::Manager;
+use clap::{command, Arg, Command};
 use log::error;
 use notify::Watcher;
 use std::process;
+use std::str::FromStr;
 use std::time::Duration;
 
 use agde_tokio::*;
+
+fn validate<T: FromStr>(validate: impl Fn(T) -> bool) -> impl Fn(&str) -> Result<(), String> {
+    move |v| {
+        if let Ok(v) = v.parse::<T>() {
+            if validate(v) {
+                Ok(())
+            } else {
+                Err("Validation failed.".into())
+            }
+        } else {
+            Err(format!(
+                "Failed to parse {v:?} into type {}",
+                std::any::type_name::<T>()
+            ))
+        }
+    }
+}
+
+fn command() -> Command<'static> {
+    let command = command!();
+    command
+        .arg(
+            Arg::new("url")
+                .short('u')
+                .long("url")
+                .default_value("ws://localhost:8081/ws")
+                .help("The WebSocket server to connect to. Requires the 'ws:' scheme."),
+        )
+        .arg(
+            Arg::new("force")
+                .short('f')
+                .long("force")
+                .help("Force overriding of local files if that's required."),
+        )
+        .arg(
+            Arg::new("sync")
+                .short('s')
+                .long("sync-interval")
+                .default_value("10")
+                .help("Number of seconds (float) to wait between committing and syncing.")
+                .validator(validate(|f: f64| f > 0.)),
+        )
+        .arg(
+            Arg::new("startup")
+                .short('w')
+                .long("startup-duration")
+                .default_value("1")
+                .help(
+                    "Number of seconds (float) to wait for piers \
+                    to welcome before fast forwarding.",
+                )
+                .validator(validate(|f: f64| f > 0.)),
+        )
+        .arg(
+            Arg::new("flush")
+                .long("flush-interval")
+                .default_value("30")
+                .help("Number of seconds (float) between writing cache to the FS.")
+                .validator(validate(|f: f64| f > 0.)),
+        )
+}
 
 #[tokio::main]
 async fn main() {
     env_logger::init();
 
-    // `TODO`: use clap for argument parsing.
-    let url = "ws://localhost:8081/ws";
+    let matches = command().get_matches();
+
+    let url = matches.value_of("url").unwrap();
+    let force = matches.is_present("force");
+
+    let sync_interval = matches
+        .value_of_t("sync")
+        .expect("--sync-interval takes a float value");
+    let startup_duration = matches
+        .value_of_t("startup")
+        .expect("--startup-duration takes a float value");
+    let flush_interval = matches
+        .value_of_t("flush")
+        .expect("--flush-interval takes a float value");
 
     loop {
-        let options = native::options_fs(false)
+        let options = native::options_fs(force)
             .await
             .expect("failed to read file system metadata");
-        let options = options.with_startup_duration(Duration::from_secs(1)).arc();
+        let options = options
+            .with_startup_duration(Duration::from_secs_f64(startup_duration))
+            .with_sync_interval(Duration::from_secs_f64(sync_interval))
+            .with_flush_interval(Duration::from_secs_f64(flush_interval))
+            .arc();
 
         let log_lifetime = Duration::from_secs(60);
 
